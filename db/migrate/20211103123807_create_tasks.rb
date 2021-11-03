011655c01cf5a9cd9f69e13a39678804e9dab4e6
Runtime <runtime@Runtimes-MBP-2.Home>
2021-11-03 14:05:00 +0000

    added Task Model

---
+++ db/migrate/20211103123807_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.datetime :date_and_time
+      t.text :description
+      t.boolean :complete
+
+      t.timestamps
+    end
+  end
+end
